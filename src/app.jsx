be93dff27f390d8a168d086c2ab62625e3f5ea4a
learning3464-cell <learning3464@gmail.com>
2025-08-25 22:08:31 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 export default function App() {
   return (
     <h1 className="text-3xl font-bold text-center mt-10">
@@ -5,4 +6,5 @@ export default function App() {
     </h1>
   );
 }
+
 
