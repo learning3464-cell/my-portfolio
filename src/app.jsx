14730d460a51d7b2677f8fdd22b9babb9e861f80
learning3464-cell <learning3464@gmail.com>
2025-08-25 22:10:05 +0530

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold text-center mt-10">
-      Hello World 🚀
-    </h1>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+      <h1 className="text-5xl font-bold text-white">Hello World 🚀</h1>
+    </div>
   );
 }
+
+
 
 
